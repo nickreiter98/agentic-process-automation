66d73b254980c1b180796317793bb625613e7c5f
nickreiter98 <nick.reiter@hotmail.de>
2024-04-18 17:24:30 +0200

    changed DirectedGraph function

--- misc/directed_graph.py
+++ misc/directed_graph.py
@@ -5,10 +5,15 @@ class DirectedGraph:
     def __init__(self):
         self.graph = nx.DiGraph()
 
-    def add_edge(self, start, end):
-        self.graph.add_edge(start, end)
+    def add_edge(self, start, end, weight='empty'):
+        if weight == 'empty':
+            self.graph.add_edge(start, end)
+        else:
+            self.graph.add_edge(start, end, weight=weight)
 
     def display(self):
-        pos = nx.spring_layout(self.graph)
-        nx.draw(self.graph, pos, with_labels=True, arrows=True)
+        pos = nx.circular_layout(self.graph)
+        nx.draw(self.graph, pos, with_labels=True, node_color='skyblue', node_size=2000, edge_color='k', width=2, arrowstyle='-|>', arrowsize=10)
+        labels = nx.get_edge_attributes(self.graph, 'weight')
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=labels)
         plt.show()

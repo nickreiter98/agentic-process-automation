951699147d59c51d158efb05c0a0bec95328071f
nickreiter98 <nick.reiter@hotmail.de>
2024-04-25 16:55:04 +0200

    changed the model representation to pm4py bpmn

--- modelling/generator.py
+++ modelling/generator.py
@@ -1,131 +1,87 @@
 from misc.directed_graph import DirectedGraph
 from modelling.notation import Node, Edge, ExclusiveEdge, StartEvent, EndEvent, Task, ExclusiveGateway, ParallelGateway
 
-# class LabeledAdjacencyMatrix:
-#     def __init__(self, labels):
-#         self.labels = labels
-#         self.size = len(labels)
-#         self.matrix = [[0 for _ in range(self.size)] for _ in range(self.size)]
-
-#     def _get_index(self, label):
-#         return self.labels.index(label)
-
-#     def add_edge(self, label1, label2, weight=1):
-#         i, j = self._get_index(label1), self._get_index(label2)
-#         self.matrix[i][j] = weight
-
-#     def remove_edge(self, label1, label2):
-#         i, j = self._get_index(label1), self._get_index(label2)
-#         self.matrix[i][j] = 0
-
-#     def has_edge(self, label1, label2):
-#         i, j = self._get_index(label1), self._get_index(label2)
-#         return self.matrix[i][j] != 0
-
-#     def get_target_nodes(self, label):
-#         i = self._get_index(label)
-#         return [self.labels[j] for j in range(self.size) if self.matrix[i][j] != 0]
-    
-#     def get_target_nodes_with_condition(self, label):
-#         i = self._get_index(label)
-#         return {self.labels[j]: self.matrix[i][j] for j in range(self.size) if self.matrix[i][j] != 0}
-
-#     def display(self):
-#         print(' ', ' '.join(self.labels))
-#         for label, row in zip(self.labels, self.matrix):
-#             print(label, row)
-
+import pm4py.objects.bpmn.obj as bpmn_obj
+from pm4py.visualization.bpmn import visualizer
 
 class ModelGenerator:
     def __init__(self):
-        self.edges = []
+        self.bpmn = bpmn_obj.BPMN()
         self.graph = {}
 
-    def start_event(self, name='Start'):
-        start_event = StartEvent(name)
-        # self.nodes.append(start_event)
+    def create_start_event(self, name='Start'):
+        start_event = self.bpmn.StartEvent(name=name)
+        self.bpmn.add_node(start_event)
         return start_event
 
-    def end_event(self, name='End'):
-        end_event = EndEvent(name)
-        # self.nodes.append(end_event)
+    def create_end_event(self, name='End'):
+        end_event = self.bpmn.EndEvent(name=name)
+        self.bpmn.add_node(end_event)
         return end_event
 
-    def task(self, name:str):
-        task = Task(name)
-        # self.nodes.append(task)
+    def create_task(self, name:str):
+        task = self.bpmn.Task(name=name)
+        self.bpmn.add_node(task)
         return task
 
-    def exclusive_gateway(self, name:str):
-        exclusive_gateway = ExclusiveGateway(name)
-        # self.nodes.append(exclusive_gateway)
+    def create_exclusive_gateway(self, name:str):
+        exclusive_gateway = self.bpmn.ExclusiveGateway(name=name, gateway_direction='diverging')
+        self.bpmn.add_node(exclusive_gateway)
         return exclusive_gateway
 
-    def parallel_gateway(self):
-        parallel_gateway = ParallelGateway()
-        # self.nodes.append(parallel_gateway)
+    def create_parallel_gateway(self, name:str):
+        parallel_gateway = self.bpmn.ParallelGateway(name='X')
+        self.bpmn.add_node(parallel_gateway)
         return parallel_gateway
-    
-    def _add_nodes_to_graph(self, origin:Node, target:Node, condition:str=None):
-        if condition == None:
-            target = (target, None)
-        else:
-            target = (target, condition)
 
-        if origin not in self.graph:
-            self.graph[origin] = [target]
-        else:
-            self.graph[origin].append(target)
+    def create_edge(self, source:Node, target:Node):
+        edge = self.bpmn.Flow(source=source, target=target)
+        self.bpmn.add_flow(edge)
+        return edge
 
-    def get_target_nodes(self, origin:Node):
-        return [target for target, _ in self.graph[origin]]
+    def create_exclusive_edge(self, source:Node, target:Node, condition:str):
+        edge = self.bpmn.Flow(source=source, target=target, name=condition)
+        self.bpmn.add_flow(edge)
+        return edge
     
-    def get_target_nodes_with_condition(self, origin:Node):
-        return {target: condition for target, condition in self.graph[origin]}
+    def is_start_event(self, node):
+        return isinstance(node, self.bpmn.StartEvent)
+    
+    def is_end_event(self, node):
+        return isinstance(node, self.bpmn.EndEvent)
+    
+    def is_task(self, node):
+        return isinstance(node, self.bpmn.Task)
+    
+    def is_exclusive_gateway(self, node):
+        return isinstance(node, self.bpmn.ExclusiveGateway)
+    
+    def is_parallel_gateway(self, node):
+        return isinstance(node, self.bpmn.ParallelGateway)
+
+    # def get_target_nodes(self, origin:Node):
+    #     return [target for target, _ in self.graph[origin]]
+    
+    # def get_target_nodes_with_condition(self, origin:Node):
+    #     return {target: condition for target, condition in self.graph[origin]}
     
     def get_start_node(self):
-        for edge in self.edges:
-            if isinstance(edge.origin, StartEvent):
-                return edge.origin
-                break
+        for node in self.bpmn.get_nodes():
+            if isinstance(node, self.bpmn.StartEvent):
+                return node.get_name()
 
-    def create_edge(self, origin:Node, target:Node):
-        edge = Edge(origin, target)
-        self.edges.append(edge)
-        self._add_nodes_to_graph(origin, target)
+    def get_graph(self):
+        return self.bpmn.get_graph()
 
-    def create_exclusive_edge(self, origin:Node, target:Node, condition:str):
-        edge = ExclusiveEdge(origin, target, condition)
-        self.edges.append(edge)
-        self._add_nodes_to_graph(origin, target, condition)
+    def view_bpmn(self):
+        gviz = visualizer.apply(self.bpmn)
+        visualizer.matplotlib_view(gviz)
 
-    def display_graph(self):
-        graph = DirectedGraph()
+    def save_bpmn(self):
+        gviz = visualizer.apply(self.bpmn)
+        visualizer.save(gviz, 'model.gviz')
 
-        for edge in self.edges:
-            if isinstance(edge, ExclusiveEdge):
-                graph.add_edge(edge.origin.name, edge.target.name, edge.condition)
-            elif isinstance(edge, Edge):
-                graph.add_edge(edge.origin.name, edge.target.name)
-        
-        graph.display()
-
-    # def get_iterable_graph(self) -> dict:
-    #     graph_dict = {}
-
-    #     for edge in self.edges:
-    #         if isinstance(edge, ExclusiveEdge):
-    #             if edge.start.name not in graph_dict:
-    #                 graph_dict[edge.start.name] = [(edge.end.name, edge.condition)]
-    #             else:
-    #                 graph_dict[edge.start.name].append((edge.end.name, edge.condition))
-    #         elif isinstance(edge, Edge):
-    #             if edge.start.name not in graph_dict:
-    #                 graph_dict[edge.start.name] = [(edge.end.name, 'empty')]
-    #             else:
-    #                 graph_dict[edge.start.name].append((edge.end.name, 'empty'))
-
-    #     return graph_dict
+    
 
 
 

ea9ab8c46eb9daee7387995c7feae1eedab7dd46
nickreiter98 <nick.reiter@hotmail.de>
2024-05-28 17:37:55 +0200

    adjusted exclusive gateway prompt

--- src/execution/prompts_exclusive_gateway.py
+++ src/execution/prompts_exclusive_gateway.py
@@ -1,24 +1,22 @@
 def add_role():
-    return  " Your role: imagine you are an exclusive gateway," \
-            " and you are able to chose the right condition given data" \
-            " to a specific context \n\n "
+    return  ("Your role: imagine you are an exclusive gateway,"
+            " and you are able to chose the right condition given data"
+            " to a specific context \n\n")
 
 def add_least_to_most():
-    return  " Select the right condition based on the given data." \
-            " For this follow the upcoming steps:\n" \
-            " - 1.: Read the data carefully and understand the context of the data to compare." \
-            " - 2.: Compare the data with the conditions and chose the right condition." \
-            " - 3.: If you cannot find the right condition, return an error message." \
-            " - 4.: If you have found the right condition, return your findings\n\n"
+    return  ("Select the right condition based on the given data." 
+            " For this follow the upcoming steps:\n" 
+            "- 1.: Read the data carefully and understand the context of the data to compare.\n" 
+            "- 2.: Compare the data with the conditions and chose the right condition.\n" 
+            "- 3.: If you cannot find the right condition, return an error message.\n" 
+            "- 4.: If you have found the right condition, return your findings\n\n")
 
 def add_output_pattern():
-    return  " Use the provided template below to return the output:\n" \
-            " {\"name\": \"condition\"}\n" \
-            " - condition: place holder for the name of the condition which is true\n" \
-            " E.g. {\"name\": \"Bank account > 10000€\"}\n" \
-            " E.g. {\"name\": \"message is spam\"}\n\n" \
-            " If you were unsuccessfull chosing a function, return" \
-            " {\"name\": \"NO CONDITION FOUND\"} \n\n"\
+    return  (" Use the provided template below to return the output:\n" 
+            " ```json\n{\"name\": \"condition\"}\n```" 
+            " - condition: place holder for the name of the condition which is true\n" 
+            " If you were unsuccessfull chosing a function, return the output:\n" 
+            " 'Condition error' \n\n")
             
 def get_sys_message():
     sys_message = add_role()
@@ -27,11 +25,13 @@ def get_sys_message():
     return sys_message
 
 def get_prompt(context:str, conditions:list[str], output:str):
-    prompt =    f" Chose the right condition based on the given data and cobtext. \n\n" \
-                f" Data: {output}" \
-                f" Context: {context} \n\n"
-    
+    prompt = "Chose the one one of the following conditions:\n"
+
     for i, condition in  enumerate(conditions):
-        prompt += f" - Condition {i+1}: {condition} \n"
-                
+        prompt += f"- Condition {i+1}: {condition}\n"
+
+    prompt += ("based on the given data and context. \n"
+               f"- Data: {output} \n" 
+               f"- Context: {context} \n")
+          
     return prompt

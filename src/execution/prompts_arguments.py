e18ece8a943d6e30f6eede96899c1a185cc798e7
nickreiter98 <nick.reiter@hotmail.de>
2024-05-28 17:38:07 +0200

    adjusted argument prompts

--- src/execution/prompts_arguments.py
+++ src/execution/prompts_arguments.py
@@ -1,30 +1,32 @@
 def add_role():
-    return  " You are an expert in function calling," \
-            " familiar with common function calling approaches." \
-            " Your task is to assign the parameters of a function with the right arguments" \
-            " The arguments can be derived from the output of the prvious function and/or from a workflow description" \
-            " Note, Previously activities from the workflow have been mapped to a function." \
-            " Also act as the process owner and use your expertise and familiarity with the" \
-            " workflow process context to avoid any irrational assigments. \n\n"
+    return  (" You are an expert in function calling," 
+            " familiar with common function calling approaches." 
+            " Your task is to assign the parameters of a function with the right arguments" 
+            " The arguments can be derived from the output of the prvious function and/or from a workflow description" 
+            " Note, Previously activities from the workflow have been mapped to a function." 
+            " Also act as the process owner and use your expertise and familiarity with the" 
+            " workflow process context to avoid any irrational assigments. \n\n")
 
 def add_knowledge():
-    return  " An parameter assignator refers to a mechanism or a process within a program" \
-            " that assigns parameters of a function with arguments. A parameter assignator" \
-            " contains following charactersitcs: \n " \
-            " - Argument Extraction: The process of identifying and extracting arguments or" \
-            " parameters passed to a specific function. Also recognizes, if parameters are optional/required\n" \
-            " - Type and Value Validation: The process of checking" \
-            " that arguments provided match the expected types as declared" \
-            " within the function description. Performing type coercion if needed. \n\n" \
+    return  (" An parameter assignator refers to a mechanism or a process within a program" 
+            " that assigns parameters of a function with arguments. A parameter assignator" 
+            " contains following charactersitcs:\n " 
+            " - Argument Extraction: The process of identifying and extracting arguments or" 
+            " parameters passed to a specific function. Also recognizes, if parameters are optional/required\n" 
+            " - Type and Value Validation: The process of checking" 
+            " that arguments provided match the expected types as declared" 
+            " within the function description. Performing type coercion if needed. \n\n")
 
 def add_least_to_most():
-    return  " Assign the parameters of the function with arguments." \
-            " I will provide a step-by-step schedule:\n" \
-            " - 1.: Assign the parameters of the function or some of them with the output of the previous function. Or with some parts of the output\n" \
-            " - 2.: Assign the parameters which haven't been assigned yet with arguments extracted from the workflow description.\n"\
-            " - 3.: If assignation is not possible, throw an Assignation error\n"\
-            " In the cases decide whether optional arguments are needed or not."\
-            " If you cannot assign the parameters, return an error message. \n\n" \
+    return  (" Assign the parameters of the function with arguments." 
+            " I will provide a step-by-step schedule:\n" 
+            "- 1.: Assign all or only the required parameters of the functions with the help of the output of the previou functions."
+            "Decide utilizing you knowledge which parameters are assigned. Try to assign the parameters by using most current output."
+            "If this is not possible, jump to the next output.\n"
+            "- 2.: Assign the parameters which haven't been assigned yet with arguments extracted from the workflow description.\n"
+            "- 3.: If assignation is not possible for the required parameters, throw an Assignation error\n"
+            "In the cases decide whether optional arguments are needed or not."
+            " If you cannot assign the parameters, return an error message. \n\n")
 
 def add_errors():
     return '''Try to avoid the following errors:
@@ -47,40 +49,42 @@ def add_errors():
                         'context_info': 'Informal, general information'}
         '''
 
-def add_function_repository(function_repository:list[dict]):
-    text = ("These are all functions from the function repository: \n"
-            "++++++++++Start Function Repository++++++++++\n")
-    
-    for i, function in enumerate(function_repository):
-        text += f"Function {i+1} --> {function} \n"
-
-    text += "++++++++++End Function Repository++++++++++\n\n"
-    return text
-
 def add_output_pattern():
-     return (" +++ TEMPLATE PATTERN +++\n" 
-             " Only provide the output in the output pattern. Dont return any explanation or reasoning."
+     return (" Only provide the output in the output pattern. Dont return any explanation or reasoning."
              " Use the provided template below to return the output: \n"
-             " {\"parameter\": \"value\", \"parameter\": \"value\"} \n"
-             " - parameter: place holder for the parameter to be assigned.\n"
-             " - value: place holder for the value to be assigned to the parameter.\n\n"
-             " +++ ERROR HANDLING +++\n"
-             " - If you were unsuccessfull assigning, return:"
-             " Assignation error \n")
+             " ```json\n{\"parameter\": \"value\", \"parameter\": \"value\"}\n```\n"
+             " - parameter: place holder for the name of the parameter to be assigned.\n"
+             " - value: place holder for the value to be assigned to the parameter.\n"
+             " Never return ```json\n{\"parameter\": \"value\"}\n```\n"
+             " If you were unsuccessfull assigning required parameters, return the output:"
+             " 'Assignation error' \n")
 
+def add_output_storage(output_storage: list[dict[dict]]) -> str:
+    if not output_storage: 
+        return "No output has been stored yet.\n"
+    else:
+        text = "The output of the previous functions are: \n"
+        for i, output in enumerate(output_storage[::-1]):
+            assert output is not None
+            function_name = list(output.keys())[0]
+            function_output = output[function_name]
+            text += (f"Function {i+1} step ago:\n"
+                    f"{function_name} with the output --> {function_output}\n")
+        return text
 
-def get_sys_message(function_repository:list[dict]):
+def get_sys_message():
     sys_message = add_role()
     sys_message += add_knowledge()
     sys_message += add_least_to_most()
     #sys_message += add_errors()
-    sys_message += add_function_repository(function_repository)
     sys_message += add_output_pattern()
     return sys_message
 
-def get_prompt(function:str, workflow:str, output:str):
-    prompt =    f" Assign the parmeter of provided function with arguments.\n" \
-                f" - Function arguments to be assigned: {function} \n"\
-                f" - Output from the previous function: {output} \n" \
-                f" - Workflow description: {workflow} \n\n"
+from typing import List
+
+def get_prompt(function:dict, workflow:str, output_storage:List[str]):
+    prompt =    f"Assign the parmeter of provided function with arguments.\n" \
+                f"- Head of the function arguments has to be assigned: {str(function)} \n"\
+                f"- {add_output_storage(output_storage)}" \
+                f"- Workflow description: {workflow} \n\n"
     return prompt

7f40852d9aa454799a7f55ccc29c03f610107055
nickreiter98 <nick.reiter@hotmail.de>
2024-05-28 17:38:44 +0200

    adjusted handler

--- src/execution/handler.py
+++ src/execution/handler.py
@@ -5,6 +5,7 @@ import re
 from src.misc import decoder
 from src.utils.open_ai import OpenAIConnection
 from src.execution import prompts_selection, prompts_arguments
+from src.repository.repository import Repository
 
 from openai import OpenAI
 from pm4py.objects.bpmn.obj import BPMN
@@ -22,66 +23,59 @@ Edge: TypeAlias = BPMN.Flow
 class FunctionSelector:
     def __init__(
             self,
-            functions: Optional[list],
+            repository: Repository,
             
     ):
         self.connection = OpenAIConnection()
-        self.list_parsed_functions = self._parse_functions(functions)
-        
-        
-    def _parse_functions(self, functions: Optional[list]) -> Optional[list]:
-        if functions is None:
-            raise Exception('Empty function repository - please provide functions to select from')
-        else:
-            return [decoder.func_to_json(func) for func in functions]
+        self.repository = repository
 
     def select(self, node:Node, workflow:str) -> str:
         sys_message = {'role': 'system',
-                        'content': prompts_selection.get_sys_message(self.list_parsed_functions)}
+                       'content': prompts_selection.get_sys_message(self.repository.get_function_to_json())}
         prompt = {'role': 'user',
-                   'content': prompts_selection.get_prompt(node, workflow)}
-        
+                  'content': prompts_selection.get_prompt(node)}
         message = [sys_message, prompt]
         response = self.connection.request(message)
-
-        if re.search('\bSelection error\b', response, re.IGNORECASE):
-            raise Exception('Mapping error - No function can be mapped to the activity')
-        else:
-            function_name = json.loads(response)
-
-        # Probably this condition is never occuring
-        if len(function_name) > 1:
-            raise Exception('Multiple functions selected - Only one function can be selected') 
-        else:
+        DICT_PATTERN = r'{(.*?)}'
+        ERROR_PATTERN = r'Selection error'
+        if re.search(ERROR_PATTERN, response, re.IGNORECASE):
+            raise Exception(f'Mapping error - No function can be mapped to the activity "{node.name}"')
+        elif re.search(DICT_PATTERN, response, re.DOTALL):
+            match = re.search(DICT_PATTERN, response, re.DOTALL).group()
+            function_name = json.loads(match)
+            assert len(function_name) == 1, ("Multiple functions selected -"
+                                             " Only one function can be selected") 
             return list(function_name.values())[0]
-        
+        else:
+            raise Exception("Neither an assignation error nor arguments could be detected"
+                            " within the response. Please try again!")
+
 class ParameterAssignator:
     def __init__(
             self,
-            functions: Optional[list],
+            repository: Repository,
     ):
          self.connection = OpenAIConnection()
-         self.list_parsed_functions = self._parse_functions(functions)
+         self.repository = repository
 
-    def _parse_functions(self, functions: Optional[list]) -> Optional[list]:
-        if functions is None:
-            return None
-        return [decoder.func_to_json(func) for func in functions]
-
-    def assign(self, function_name:str, output:str, workflow:str) -> str:
+    def assign(self, function_name:str, workflow:str, output_storage) -> str:
         sys_message = {'role': 'system', 
-                       'content': prompts_arguments.get_sys_message(self.list_parsed_functions)}
+                       'content': prompts_arguments.get_sys_message()}
         prompt = {'role': 'user', 
-                  'content': prompts_arguments.get_prompt(function_name, workflow, str(output))}
+                  'content': prompts_arguments.get_prompt(self.repository.get_json_by_name(function_name),
+                                                          workflow,
+                                                          output_storage)}
         message = [sys_message, prompt]
         response = self.connection.request(message)
 
         DICT_PATTERN = r'{(.*?)}'
         ERROR_PATTERN = r'Assignation error'
         if re.search(ERROR_PATTERN, response, re.IGNORECASE):
-            raise Exception('Assignation error - No parameters can be assigned to the function')
+            raise Exception(f'Assignation error - No parameters can be assigned to the function "{function_name}"')
         elif re.search(DICT_PATTERN, response, re.DOTALL):
-            match = re.search(DICT_PATTERN, response, re.DOTALL)
-            return match.group()
+            match = re.search(DICT_PATTERN, response, re.DOTALL).group()
+            arguments = json.loads(match)
+            return arguments
         else:
-            return response
+            raise Exception("Neither an assignation error nor arguments could be detected"
+                            " within the response. Please try again!")

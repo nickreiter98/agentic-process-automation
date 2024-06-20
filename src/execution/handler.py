0b443778d163e06adcf4562687a29df3b663a4f3
nickreiter98 <nick.reiter@hotmail.de>
2024-06-20 15:58:37 +0200

    made handler PEP8 conform

--- src/execution/handler.py
+++ src/execution/handler.py
@@ -11,6 +11,7 @@ from pm4py.objects.bpmn.obj import BPMN
 
 # Define type hints
 from typing import Optional, TypeAlias
+
 StartEvent: TypeAlias = BPMN.StartEvent
 EndEvent: TypeAlias = BPMN.EndEvent
 Task: TypeAlias = BPMN.Task
@@ -18,63 +19,78 @@ ExclusiveGateway: TypeAlias = BPMN.ExclusiveGateway
 ParallelGateway: TypeAlias = BPMN.ParallelGateway
 Node: TypeAlias = BPMN.BPMNNode
 Edge: TypeAlias = BPMN.Flow
-    
+
+
 class FunctionSelector:
-    def __init__(
-            self,
-            repository: Repository,
-            
-    ):
-        self.connection = OpenAIConnection()
+    def __init__(self, repository: Repository):
+        self.llm_connection = OpenAIConnection()
         self.repository = repository
 
-    def select(self, node:Node, workflow:str) -> str:
-        sys_message = {'role': 'system',
-                       'content': prompt_selection.get_sys_message(self.repository.retrieve_json_representations())}
-        prompt = {'role': 'user',
-                  'content': prompt_selection.get_prompt(node)}
+    def select(self, node: Node, workflow: str) -> str:
+        sys_message = {
+            "role": "system",
+            "content": prompt_selection.get_sys_message(
+                self.repository.retrieve_json_representations()
+            )
+        }
+        prompt = {"role": "user", "content": prompt_selection.get_prompt(node)}
         message = [sys_message, prompt]
-        response = self.connection.request(message)
-        DICT_PATTERN = r'{(.*?)}'
-        ERROR_PATTERN = r'Selection error'
+        response = self.llm_connection.request(message)
+        DICT_PATTERN = r"{(.*?)}"
+        ERROR_PATTERN = r"Selection error"
         if re.search(ERROR_PATTERN, response, re.IGNORECASE):
-            raise Exception(f'Mapping error - No interface can be mapped to the activity "{node.name}"')
+            raise Exception(
+                f"Mapping error - No interface can be mapped "
+                f"to the activity '{node.name}'"
+            )
         elif re.search(DICT_PATTERN, response, re.DOTALL):
             match = re.search(DICT_PATTERN, response, re.DOTALL).group()
             interface = json.loads(match)
-            assert len(interface) == 1, ("Multiple interfaces selected -"
-                                             " Only one interface can be selected") 
+            assert len(interface) == 1, (
+                "Multiple interfaces selected - Only one interface can be selected"
+            )
             return list(interface.values())[0]
         else:
-            raise Exception("Neither an assignation error nor arguments could be detected"
-                            " within the response. Please try again!")
+            raise Exception(
+                "Neither an assignation error nor arguments could be detected "
+                "within the response. Please try again!"
+            )
+
 
 class ParameterAssignator:
-    def __init__(
-            self,
-            repository: Repository,
-    ):
-         self.connection = OpenAIConnection()
-         self.repository = repository
+    def __init__(self, repository: Repository):
+        self.llm_connection = OpenAIConnection()
+        self.repository = repository
 
-    def assign(self, interface:str, workflow:str, output_storage) -> str:
-        sys_message = {'role': 'system', 
-                       'content': prompt_arguments.get_sys_message()}
-        prompt = {'role': 'user', 
-                  'content': prompt_arguments.get_prompt(self.repository.retrieve_json_representation_by_name(interface),
-                                                          workflow,
-                                                          output_storage)}
+    def assign(self, interface: str, workflow: str, output_storage) -> str:
+        sys_message = {
+            "role": "system",
+            "content": prompt_arguments.get_sys_message()
+        }
+        prompt = {
+            "role": "user",
+            "content": prompt_arguments.get_prompt(
+                self.repository.retrieve_json_representation_by_name(interface),
+                workflow,
+                output_storage,
+            )
+        }
         message = [sys_message, prompt]
-        response = self.connection.request(message)
+        response = self.llm_connection.request(message)
 
-        DICT_PATTERN = r'{(.*?)}'
-        ERROR_PATTERN = r'Assignation error'
+        DICT_PATTERN = r"{(.*?)}"
+        ERROR_PATTERN = r"Assignation error"
         if re.search(ERROR_PATTERN, response, re.IGNORECASE):
-            raise Exception(f'Assignation error - No parameters can be assigned to the interface "{interface}"')
+            raise Exception(
+                f"Assignation error - No parameters can be assigned"
+                f"to the interface '{interface}'"
+            )
         elif re.search(DICT_PATTERN, response, re.DOTALL):
             match = re.search(DICT_PATTERN, response, re.DOTALL).group()
             arguments = json.loads(match)
             return arguments
         else:
-            raise Exception("Neither an assignation error nor arguments could be detected"
-                            " within the response. Please try again!")
+            raise Exception(
+                "Neither an assignation error nor arguments could be detected "
+                "within the response. Please try again!"
+            )

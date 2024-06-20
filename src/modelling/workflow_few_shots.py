41c1a91aa4659b9593e8c92b68777dfc37b934c7
nickreiter98 <nick.reiter@hotmail.de>
2024-06-20 11:38:23 +0200

    formatted workflow_few_shhots after PEP8

--- src/modelling/workflow_few_shots.py
+++ src/modelling/workflow_few_shots.py
@@ -1,22 +1,25 @@
 from src.modelling.generator import ModelGenerator
-                       
-d1 = ("The user has to register for the app. After registering the user can"
-    " chose a product and add it to the cart. The the user is checking out and pays the"
-    " product. If the payment was successfull, the product is shipped. if no,"
-    " the transaction is aborted.")
+
+d1 = (
+    "The user has to register for the app. After registering the user can"
+    " chose a product and add it to the cart. The the user is checking out"
+    " and pays the product. If the payment was successfull, the product is"
+    " shipped. if no, the transaction is aborted."
+)
+
 
 def m1():
     model = ModelGenerator()
 
     start = model.create_start_event()
-    register = model.create_task('register for app')
-    chose_product = model.create_task('chose product')
-    add_product = model.create_task('add product to cart')
-    checkout = model.create_task('checkout')
-    payment = model.create_task('payment')
-    payment_successfull = model.create_exclusive_gateway('payment successfull')
-    transaction_aborted = model.create_task('transaction aborted')
-    start_shipping = model.create_task('start shipping')
+    register = model.create_task("register for app")
+    chose_product = model.create_task("chose product")
+    add_product = model.create_task("add product to cart")
+    checkout = model.create_task("checkout")
+    payment = model.create_task("payment")
+    payment_successful = model.create_exclusive_gateway("payment successful")
+    transaction_aborted = model.create_task("transaction aborted")
+    start_shipping = model.create_task("start shipping")
     end_1 = model.create_end_event()
     end_2 = model.create_end_event()
 
@@ -25,25 +28,31 @@ def m1():
     model.create_edge(chose_product, add_product)
     model.create_edge(add_product, checkout)
     model.create_edge(checkout, payment)
-    model.create_edge(payment, payment_successfull)
-    model.create_exclusive_edge(payment, transaction_aborted, 'payment failed')
+    model.create_edge(payment, payment_successful)
+    model.create_exclusive_edge(payment, transaction_aborted, "payment failed")
     model.create_edge(transaction_aborted, end_2)
-    model.create_exclusive_edge(payment_successfull, start_shipping, 'payment successfull')
+    model.create_exclusive_edge(
+        payment_successful, start_shipping, "payment successful"
+    )
     model.create_edge(start_shipping, end_1)
-             
+
     model.initialize()
 
-d2 = ("Get the bank account of user 214423. Write an email to the user and"
-    " also send a message to the support team.")
+
+d2 = (
+    "Get the bank account of user 214423. Write an email to the user and"
+    " also send a message to the support team."
+)
+
 
 def m2():
     model = ModelGenerator()
-    
+
     start = model.create_start_event()
-    bank_account = model.create_task('get bank account of user 214423')
-    parallel_gateway_1 = model.create_parallel_gateway('1')
-    write_email = model.create_task('write email to user')
-    send_message = model.create_task('send message to support team')
+    bank_account = model.create_task("get bank account of user 214423")
+    parallel_gateway_1 = model.create_parallel_gateway("1")
+    write_email = model.create_task("write email to user")
+    send_message = model.create_task("send message to support team")
     end_1 = model.create_end_event()
     end_2 = model.create_end_event()
 
@@ -56,23 +65,29 @@ def m2():
 
     model.initialize()
 
-d3 = ("Extract information from the wikipedia page about python (programming language)."
-      " Then write a summary of the information. Afterwards you are sending the summary"
-      " via email, Slack and also store it on your local machine. After that you are"
-      " checking whether the the sending was successfull")
+
+d3 = (
+    "Extract information from the wikipedia page about python (programming language)."
+    " Then write a summary of the information. Afterwards you are sending the summary"
+    " via email, Slack and also store it on your local machine. After that you are"
+    " checking whether the sending was successful"
+)
+
 
 def m3():
     model = ModelGenerator()
 
     start = model.create_start_event()
-    extract_info = model.create_task('extract information from wikipedia page about python')
-    write_summary = model.create_task('write summary of information')
-    parallel_gateway_1 = model.create_parallel_gateway('1')
-    send_email = model.create_task('send summary via email')
-    send_slack = model.create_task('send summary via Slack')
-    store_local = model.create_task('store summary on local machine')
-    check_sending_1 = model.create_task('check whether sending was successfull')
-    check_sending_2 = model.create_task('check whether sending was successfull')
+    extract_info = model.create_task(
+        "extract information from wikipedia page about python"
+    )
+    write_summary = model.create_task("write summary of information")
+    parallel_gateway_1 = model.create_parallel_gateway("1")
+    send_email = model.create_task("send summary via email")
+    send_slack = model.create_task("send summary via Slack")
+    store_local = model.create_task("store summary on local machine")
+    check_sending_1 = model.create_task("check whether sending was successful")
+    check_sending_2 = model.create_task("check whether sending was successful")
     end_1 = model.create_end_event()
     end_2 = model.create_end_event()
     end_3 = model.create_end_event()
@@ -91,18 +106,23 @@ def m3():
 
     model.initialize()
 
-d4 = ("The process is started with A. After A, B and C are executed. After both of them D is following")
+
+d4 = (
+    "The process is started with A. After A, B and C are executed. After both"
+    " of them D is following"
+)
+
 
 def m4():
     model = ModelGenerator()
 
     start = model.create_start_event()
-    a = model.create_task('A')
-    parallel_gateway_1 = model.create_parallel_gateway('1')
-    b = model.create_task('B')
-    c = model.create_task('C')
-    d1 = model.create_task('D')
-    d2 = model.create_task('D')
+    a = model.create_task("A")
+    parallel_gateway_1 = model.create_parallel_gateway("1")
+    b = model.create_task("B")
+    c = model.create_task("C")
+    d1 = model.create_task("D")
+    d2 = model.create_task("D")
     end_1 = model.create_end_event()
     end_2 = model.create_end_event()
 
@@ -117,16 +137,21 @@ def m4():
 
     model.initialize()
 
-de1 = "First, register for the app. Then access the app. After that, add a product to the cart."
+
+de1 = (
+    "First, register for the app. Then access the app. After that, add a"
+    " product to the cart."
+)
+
 
 def me1():
     model = ModelGenerator()
 
     start = model.create_start_event()
-    register = model.create_task('register for app')
-    access_app = model.create_task('access app')
-    parallel_gateway_1 = model.create_parallel_gateway('1')
-    add_product = model.create_task('add product to cart')
+    register = model.create_task("register for app")
+    access_app = model.create_task("access app")
+    parallel_gateway_1 = model.create_parallel_gateway("1")
+    add_product = model.create_task("add product to cart")
     end = model.create_end_event()
 
     model.create_edge(start, register)
@@ -134,22 +159,28 @@ def me1():
     model.create_edge(access_app, parallel_gateway_1)
     model.create_edge(parallel_gateway_1, add_product)
     model.create_edge(parallel_gateway_1, end)
-             
+
     model.initialize()
 
-ee1 = ("This process model violates the rule that every end of a process stream"
-    " must end with an end event. Also the parallel gateway is not used correctly"
-    " since the workflow is clearly sequential")
 
-de2 = ("First at all, try to get the status of user ticket 123455. Then send the"
-       " status of the ticket to the user and also to the support team.")
+ee1 = (
+    "This process model violates the rule that every end of a process stream"
+    " must end with an end event. Also the parallel gateway is not used correctly"
+    " since the workflow is clearly sequential"
+)
+
+de2 = (
+    "First at all, try to get the status of user ticket 123455. Then send the"
+    " status of the ticket to the user and also to the support team."
+)
+
 
 def me2():
     model = ModelGenerator()
 
     start = model.create_start_event()
-    get_status = model.create_task('get status of user ticket 123455')
-    send_status = model.create_task('send status to user and to support team')
+    get_status = model.create_task("get status of user ticket 123455")
+    send_status = model.create_task("send status to user and to support team")
     end_1 = model.create_end_event()
 
     model.create_edge(start, get_status)
@@ -158,24 +189,30 @@ def me2():
 
     model.initialize()
 
-ee2 = ("In this process to activities are combined in one task."
-       "This must be avoided under all circumstances. The proper way to "
-       "to model this process is to split the task into two separate tasks"
-       " and connect them with a parallel gateway.")
 
-de3 = ("In this process, start with A. Afterwards, B is executed. After B,"
-       " C and D are simultaneously executed. After C and D, E is executed.")
+ee2 = (
+    "In this process two activities are combined in one task. This must be"
+    " avoided under all circumstances. The proper way to model this process"
+    " is to split the task into two separate tasks and connect them with a"
+    " parallel gateway."
+)
+
+de3 = (
+    "In this process, start with A. Afterwards, B is executed. After B, C and"
+    " D are simultaneously executed. After C and D, E is executed."
+)
+
 
 def me3():
     model = ModelGenerator()
 
     start = model.create_start_event()
-    a = model.create_task('A')
-    b = model.create_task('B')
-    parallel_gateway_1 = model.create_parallel_gateway('1')
-    c = model.create_task('C')
-    d = model.create_task('D')
-    e = model.create_task('E')
+    a = model.create_task("A")
+    b = model.create_task("B")
+    parallel_gateway_1 = model.create_parallel_gateway("1")
+    c = model.create_task("C")
+    d = model.create_task("D")
+    e = model.create_task("E")
     end = model.create_end_event()
 
     model.create_edge(start, a)
@@ -187,25 +224,31 @@ def me3():
     model.create_edge(d, e)
     model.create_edge(e, end)
 
-ee3 = ("In this process, the parallel branches are not modelled correctly."
-       " After C and D, both parallel branches are converging to E."
-       " Since no converging of parallel streams is possible, E must be"
-       " duplicated into two edges and connected to C and D separately.")
 
-de4 = ("Firstly, A is triggered. A is followed by B."
-       "  To end the process, C and D are triggered")
+ee3 = (
+    "In this process, the parallel branches are not modelled correctly."
+    " After C and D, both parallel branches are converging to E. Since no"
+    " converging of parallel streams is possible, E must be duplicated into"
+    " two edges and connected to C and D separately."
+)
+
+de4 = (
+    "Firstly, A is triggered. A is followed by B. To end the process, C and"
+    " D are triggered"
+)
+
 
 def me4():
     model = ModelGenerator()
 
     start = model.create_start_event()
-    a = model.create_task('A')
-    b = model.create_task('B')
-    parallel_gateway_1 = model.create_parallel_gateway('1')
-    c = model.create_task('C')
-    parallel_gateway_1a = model.create_parallel_gateway('1a')
-    d = model.create_task('D')
-    parallel_gateway_1b = model.create_parallel_gateway('1b')
+    a = model.create_task("A")
+    b = model.create_task("B")
+    parallel_gateway_1 = model.create_parallel_gateway("1")
+    c = model.create_task("C")
+    parallel_gateway_1a = model.create_parallel_gateway("1a")
+    d = model.create_task("D")
+    parallel_gateway_1b = model.create_parallel_gateway("1b")
     end_1 = model.create_end_event()
     end_2 = model.create_end_event()
 
@@ -221,10 +264,15 @@ def me4():
 
     model.initialize()
 
-ee4 = ("In this process, the parallle gatways '1a' and '1b' are not used correctly."
-       " Normally, parallel gateways are used for diverging a process stream."
-       " In this case, the parallel gateways are unnecessary and can be removed."
-       " Als an end event must follow a task and not a parallel or exclusive gateway.")
+
+ee4 = (
+    "In this process, the parallel gateways '1a' and '1b' are not used"
+    " correctly. Normally, parallel gateways are used for diverging a process"
+    " stream. In this case, the parallel gateways are unnecessary and can be"
+    " removed. Also, an end event must follow a task and not a parallel or"
+    " exclusive gateway."
+)
 
 SHOTS = [(d1, m1), (d2, m2), (d3, m3), (d4, m4)]
-SHOTS_WITH_ERRORS = [(de1, me1, ee1), (de2, me2, ee2), (de3, me3, ee3), (de4, me4, ee4)]
+SHOTS_WITH_ERRORS = [(de1, me1, ee1), (de2, me2, ee2),
+                     (de3, me3, ee3), (de4, me4, ee4)]

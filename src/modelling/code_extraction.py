bea140c5e4faabfb926cc78564d5610007a4d544
nickreiter98 <nick.reiter@hotmail.de>
2024-06-21 11:23:27 +0200

    commented code extraction

--- src/modelling/code_extraction.py
+++ src/modelling/code_extraction.py
@@ -1,6 +1,16 @@
 import re
 
 def extract_final_python_code(response_text:str) -> str:
+    """Extracts the final Python code snippet from the response text from LLM
+    and returns an executable Python code snippet.
+    Also checks whether the import statement is correct.
+
+    :param response_text: the response text from the LLM
+    :raises Exception: if the Python code snippet does not meet the import statement requirements
+    :raises Exception: if no Python code snippet is found
+    :return: executable Python code snippet
+    """
+    # Define the pattern for the Python code snippet
     python_code_pattern = r"```python(.*?)```"
     allowed_import_path = "src.modelling.workflow_processor"
     allowed_import_class = "WorkflowProcessor"
@@ -16,13 +26,18 @@ def extract_final_python_code(response_text:str) -> str:
         + re.escape(allowed_import_class)
         + r")\s*$"
     )
-    
+
+    # Find Python code snippet in the response text
     matches = re.findall(python_code_pattern, response_text, re.DOTALL)
 
-    if matches:
+    # Check if python code snippet is found
+    if matches: 
+        # Remove leading and trailing whitespaces 
         python_snippet = matches[-1].strip()
+        # Split the Python code snippet into lines
         lines = python_snippet.split('\n')
         for line in lines:
+            # Check if the import statement is comprised in the Python code snippet
             if re.match(any_import_pattern, line):
                 if not re.match(allowed_import_pattern, line):
                     raise Exception(
@@ -32,5 +47,6 @@ def extract_final_python_code(response_text:str) -> str:
                         f"from {allowed_import_path} import {allowed_import_class}"
                     )
         return python_snippet
+    # No Python code snippet found
     else:
         raise Exception("No Python code snippet found!")

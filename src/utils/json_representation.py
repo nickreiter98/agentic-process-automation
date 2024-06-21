dd27fbf32e7b502185feed318f96e6723b223aa5
nickreiter98 <nick.reiter@hotmail.de>
2024-06-21 14:22:22 +0200

    made utils PEP8 conform

--- src/utils/json_representation.py
+++ src/utils/json_representation.py
@@ -4,12 +4,15 @@ import re
 
 from typing import Literal
 
-def convert_function_to_json_representation(func, format:str = 'normal'):
+
+def convert_function_to_json_representation(func, format: str = "normal"):
     # Check if the function is a functools.partial
     if isinstance(func, functools.partial) or isinstance(func, functools.partialmethod):
         fixed_args = func.keywords
         _func = func.func
-        if isinstance(func, functools.partial) and (fixed_args is None or fixed_args == {}):
+        if isinstance(func, functools.partial) and (
+            fixed_args is None or fixed_args == {}
+        ):
             fixed_args = dict(zip(func.func.__code__.co_varnames, func.args))
     else:
         fixed_args = {}
@@ -22,7 +25,9 @@ def convert_function_to_json_representation(func, format:str = 'normal'):
     # get the function docstring
     func_doc = inspect.getdoc(_func)
     # parse the docstring to get the description
-    func_description = ''.join([line for line in func_doc.split("\n") if not line.strip().startswith(':')])
+    func_description = "".join(
+        [line for line in func_doc.split("\n") if not line.strip().startswith(":")]
+    )
     # parse the docstring to get the descriptions for each parameter in dict format
     param_details = extract_params(func_doc) if func_doc else {}
     # attach parameter types to params and exclude fixed args
@@ -32,44 +37,38 @@ def convert_function_to_json_representation(func, format:str = 'normal'):
         if param_name not in fixed_args.keys():
             params[param_name] = {
                 "description": param_details.get(param_name) or "",
-                "type": map_type(argspec.annotations.get(param_name, type(None)))
+                "type": map_type(argspec.annotations.get(param_name, type(None))),
             }
     # calculate required parameters excluding fixed args
     # _required = [arg for arg in argspec.args if arg not in fixed_args]
     _required = [i for i in argspec.args if i not in fixed_args.keys()]
     sig = inspect.signature(_func)
     requ_params = sig.parameters
-    _required = [name for name, param in requ_params.items() if param.default == inspect.Parameter.empty]
-    # if inspect.getfullargspec(_func).defaults:
-    #     _required = [argspec.args[i] for i, a in enumerate(argspec.args) if
-    #                 argspec.args[i] not in inspect.getfullargspec(_func).defaults and argspec.args[
-    #                     i] not in fixed_args.keys()]
-    # then return everything in dict
+    _required = [
+        name
+        for name, param in requ_params.items()
+        if param.default == inspect.Parameter.empty
+    ]
 
-    if format == 'normal':
+    if format == "normal":
         return {
             "name": func_name,
             "description": func_description,
-            "parameters": {
-                "type": "object",
-                "properties": params
-            },
-            "required": _required
+            "parameters": {"type": "object", "properties": params},
+            "required": _required,
         }
-    elif format == 'arguments':
-        return {
-            "parameters": params,
-            "required": _required
-        }
-    
+    elif format == "arguments":
+        return {"parameters": params, "required": _required}
+
+
 def extract_params(doc_str: str):
     # split doc string by newline, skipping empty lines
     params_str = [line for line in doc_str.split("\n") if line.strip()]
     params = {}
     for line in params_str:
         # we only look at lines starting with ':param'
-        if line.strip().startswith(':param'):
-            param_match = re.findall(r'(?<=:param )\w+', line)
+        if line.strip().startswith(":param"):
+            param_match = re.findall(r"(?<=:param )\w+", line)
             if param_match:
                 param_name = param_match[0]
                 desc_match = line.replace(f":param {param_name}:", "").strip()
@@ -78,6 +77,7 @@ def extract_params(doc_str: str):
                     params[param_name] = desc_match
     return params
 
+
 def map_type(dtype):
     if dtype == float:
         return "number"
@@ -85,7 +85,7 @@ def map_type(dtype):
         return "integer"
     elif dtype == str:
         return "string"
-    elif getattr(dtype, '__origin__', None) is Literal:
+    elif getattr(dtype, "__origin__", None) is Literal:
         return list(dtype.__args__)
     elif re.match(r"<module '(.+)' from", str(dtype)) is not None:
         match = re.match(r"<module '(.+)' from", str(dtype)).group()
--- src/utils/open_ai.py
+++ src/utils/open_ai.py
@@ -4,16 +4,16 @@ from dotenv import load_dotenv
 
 from typing import List, Dict
 
-load_dotenv('.env')
+load_dotenv(".env")
 
-class OpenAIConnection():
+
+class OpenAIConnection:
     def __init__(self):
-        self.model = os.environ['OPENAI_MODEL']
-        self.client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+        self.model = os.environ["OPENAI_MODEL"]
+        self.client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
     def request(self, messages: List[Dict[str, str]]) -> str:
         response = self.client.chat.completions.create(
-            model=self.model,
-            messages=messages
+            model=self.model, messages=messages
         )
         return response.choices[0].message.content

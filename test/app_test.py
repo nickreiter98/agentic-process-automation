a69b8c1bf8931eb04232affc6625a58a7359123c
nickreiter98 <nick.reiter@hotmail.de>
2024-06-21 15:07:57 +0200

    commented test class

--- test/app_test.py
+++ test/app_test.py
@@ -14,6 +14,7 @@ from src.modelling.workflow_generation import generate_workflow
 
 def render_pdf(current_time, name, workflow, workflow_str, workflow_image, execution_text, folder_path, iterations):
    env = Environment(loader=FileSystemLoader('.'))
+   # Get the rendering template and render it
    template = env.get_template("test/test_template.html")
    rendered_template = template.render(
       current_time=current_time,
@@ -29,6 +30,7 @@ def render_pdf(current_time, name, workflow, workflow_str, workflow_image, execu
    pdf.close()
    pisa_status.err
 
+   # Delete the workflow image and gviz file
    try:
       os.remove(workflow_image)
       os.remove(workflow_image + '.png')
@@ -37,31 +39,37 @@ def render_pdf(current_time, name, workflow, workflow_str, workflow_image, execu
 
 if __name__ == '__main__':
    current_time = datetime.today().strftime('%Y_%m_%d-%H:%M:%S')
+   # If the folder does not exist, create it
    if not os.path.exists(f'test/{current_time}'):
       os.makedirs(f'test/{current_time}')
    folder_path = f'test/{current_time}'
 
    workflows = pd.read_csv('test/workflow_descriptions.csv', index_col=0)
 
+   # Iterate over the workflows
    for index, row in workflows.iterrows():
       content = row['content']
       workflow_str = ''
       workflow_image = ''
       execution_log = ''
       try:
+         # Generate the workflow
          workflow, iterations = generate_workflow(content, 10)
          workflow_str = workflow.__str__()
          worklfow_gviz = workflow.get_bpmn()
          workflow_image = f'{folder_path}/{index}'
          worklfow_gviz.render(workflow_image, format='png')
          try:
+            # Execute the workflow
             executer = WorkflowExecutor(content, workflow)
             executer.run()
             execution_log = executer.get_log()
             render_pdf(current_time, index, content, workflow_str, workflow_image, execution_log, folder_path, iterations)
+         # If an error occurs during the execution, render the pdf with the error
          except Exception as e:
             error = f'<span style="color: red;">{type(e).__name__}: {str(e)}</span>'
             render_pdf(current_time, index, content, workflow_str, workflow_image, error, folder_path, iterations)
+      # If an error occurs during the generation, render the pdf with the error
       except Exception as e:
          error = f'<span style="color: red;">{type(e).__name__}: {str(e)}</span>'
          render_pdf(current_time, index, content, error, workflow_image, execution_log, folder_path, iterations)
